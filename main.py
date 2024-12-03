fbcd55d24649776e8bff4d56f0e2432118d75add
techno Sage <fletchercarlton2001@gmail.com>
2024-12-03 15:16:35 +1000

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+class Admin:
+    class apps:pass
+    class beings:pass
+    class code:pass
+
+# auto discord
+# social medai
+# self dev
+
+def thinking(metric):
+    while(metric < 1):
+        for i in range(10): 
+            pass
